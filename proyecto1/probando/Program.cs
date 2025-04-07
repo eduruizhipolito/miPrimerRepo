081495333f0e1972f5073fe586e43687e90c3f6e
eduruizhipolito <eduruizhipolito@gmail.com>
2025-04-07 09:36:40 -0500

    cadena nueva

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Team!");
 
 string stripe_api_key="sk_test_sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+string testingConnection = "Server:tcp:eduserver.database.windows.net,1433;Initial Catalog=TestDB;User ID=sa;Password=your_password;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
